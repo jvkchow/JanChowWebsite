6b42c3d209ddf65f4e285bb4e28cd048ec9ca700
Jan Chow <97210124+jvkchow@users.noreply.github.com>
2022-10-09 18:04:37 -0600

    fix typos

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$(document).read(function(){
+$(document).ready(function(){
     $(window).scroll(function(){
-        if(this.scrollY() > 20) {
+        if(this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.sroll > 500) {
+        if(this.scrollY > 500) {
             $('.scroll-up-button').addClass("show");
         }
         else{
